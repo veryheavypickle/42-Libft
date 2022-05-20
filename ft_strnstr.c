7a9f7cfac3f199a2ff308d8cd0ae6813df4f067e
veryheavypickle <xavier.carroll01@gmail.com>
2022-05-20 05:02:37 +0200

    Updated strnstr gang gang

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -12,23 +12,27 @@
 
 #include "libft.h"
 
-char	*ft_strnstr(char *str, char *to_find, size_t max)
+char	*ft_strnstr(const char *str, const char *to_find, size_t max)
 {
-	int	i;
-	int	j;
+	size_t	i;
+	size_t	j;
+    size_t  len_str;
+    char *whole_text;
 
 	i = 0;
 	j = 0;
+    len_str = ft_strlen(to_find);
+    whole_text = (char *)str;
 	if (to_find[j] == '\0')
-		return (str);
-	while (str[i] != '\0' && i >= (int)max)
-	{
-		while (str[i + j] == to_find[j] && str[i + j] != '\0')
-			j++;
-		if (to_find[j] == '\0')
-			return (str + i);
-		i++;
-		j = 0;
-	}
-	return (0);
+		return (whole_text);
+    while (whole_text[i] != '\0')
+    {
+        j = 0;
+        while (whole_text[i + j] != '\0' && to_find[j] != '\0' && whole_text[i + j] == to_find[j] && i + j < max)
+            j++;
+        if (j == len_str)
+            return (whole_text + i);
+        i++;
+    }
+    return (0);
 }
