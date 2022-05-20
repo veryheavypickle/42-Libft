c6c458a9b95552b0d6daa03eefff2d37c6f06da6
veryheavypickle <xavier.carroll01@gmail.com>
2022-05-20 07:12:51 +0200

    I forgot to remove previous cat lol

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -12,28 +12,6 @@
 
 #include "libft.h"
 
-
-/*
-size_t	ft_strlcat(char *dest, const char *src, size_t size)
-{
-    size_t len_dest;
-    size_t len_src;
-    size_t i_src;
-    size_t i_dest;
-
-    i_src = 0;
-    i_dest = 0;
-    len_dest = ft_strlen(dest);
-    len_src = ft_strlen(src);
-    if (size < len_dest)
-        return (len_src + size);
-    while (src[i_src] && i_src < size - len_dest - 1)
-        dest[i_dest++] = src[i_src++];
-    dest[i_dest] = 0;
-    return (len_dest + len_src);
-}
- */
-
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
     size_t len_dest;
