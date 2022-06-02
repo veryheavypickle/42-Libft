5135dd01b070a83f8ef9d6696d2e0985bdf58c93
veryheavypickle <xavier.carroll01@gmail.com>
2022-06-02 20:10:00 +0200

    man idk wtf is goin on with memmove but it wont pass

--- ft_memmove.c
+++ ft_memmove.c
@@ -2,19 +2,17 @@
 
 void *ft_memmove(void *dst, const void *src, size_t len)
 {
-    char *d;
-    char *s;
+    char    *d;
+    char	*s;
 
     d = (char *)dst;
     s = (char *)src;
-    if (!dst || !src)
-        return (NULL);
     if (d < s)
         return (ft_memcpy(dst, src, len));
     else
     {
         while (len--)
             *(d + len) = *(s + len);
-        return (dst);
     }
+    return (dst);
 }
