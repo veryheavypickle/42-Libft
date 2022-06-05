395329fc61823a91ab57e0f467008800210e06ec
veryheavypickle <xavier.carroll01@gmail.com>
2022-06-05 16:38:00 +0200

    norminette is no longer offended

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,32 +6,37 @@
 /*   By: xcarroll <xcarroll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 22:00:52 by xcarroll          #+#    #+#             */
-/*   Updated: 2022/06/05 15:55:47 by xcarroll         ###   ########.fr       */
+/*   Updated: 2022/06/05 16:37:11 by xcarroll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+due to me not wanting to offend norminette (for a line too long)
+I changed the good name I had for variable t from 'whole_text'
+t - whole text pointer
+*/
 char	*ft_strnstr(const char *str, const char *to_find, size_t max)
 {
 	size_t	i;
 	size_t	j;
 	size_t	len_str;
-	char	*whole_text;
+	char	*t;
 
 	i = 0;
 	j = 0;
 	len_str = ft_strlen(to_find);
-	whole_text = (char *)str;
+	t = (char *)str;
 	if (to_find[j] == '\0')
-		return (whole_text);
-	while (whole_text[i] != '\0')
+		return (t);
+	while (t[i] != '\0')
 	{
 		j = 0;
-		while (whole_text[i + j] && to_find[j] && whole_text[i + j] == to_find[j] && i + j < max)
+		while (t[i + j] && to_find[j] && t[i + j] == to_find[j] && i + j < max)
 			j++;
 		if (j == len_str)
-			return (whole_text + i);
+			return (t + i);
 		i++;
 	}
 	return (0);
