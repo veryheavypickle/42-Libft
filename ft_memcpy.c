70aef1f765778a08c3a39ba9354c414a99b2798c
veryheavypickle <xavier.carroll01@gmail.com>
2022-06-05 17:44:04 +0200

    updated memcpy to pass unit-tester

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,12 +6,13 @@
 /*   By: xcarroll <xcarroll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 15:51:22 by xcarroll          #+#    #+#             */
-/*   Updated: 2022/06/05 16:44:03 by xcarroll         ###   ########.fr       */
+/*   Updated: 2022/06/05 17:43:42 by xcarroll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
 void	*ft_memcpy(void *dest_any, const void *src_any, size_t size)
 {
 	char	*dest;
@@ -22,4 +23,20 @@ void	*ft_memcpy(void *dest_any, const void *src_any, size_t size)
 	while (size--)
 		dest[size] = src[size];
 	return (dest);
+}
+*/
+
+void	*ft_memcpy(void *dest_any, const void *src_any, size_t size)
+{
+	int	i;
+
+	i = 0;
+	if ((!dest_any && !src_any) || !size)
+		return (dest_any);
+	while (size--)
+	{
+		((char *)dest_any)[i] = ((char *)src_any)[i];
+		i++;
+	}
+	return (dest_any);
 }
