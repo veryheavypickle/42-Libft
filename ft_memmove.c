17b4ba6681a1e2c430bfb6ba3118c9bf1b1a13da
veryheavypickle <xavier.carroll01@gmail.com>
2022-06-05 15:49:41 +0200

    norm

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,18 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xcarroll <xcarroll@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/05 15:48:08 by xcarroll          #+#    #+#             */
+/*   Updated: 2022/06/05 15:48:09 by xcarroll         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void *ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-    char    *d;
-    char	*s;
+	char	*d;
+	char	*s;
 
-    d = (char *)dst;
-    s = (char *)src;
-    if (d < s)
-        return (ft_memcpy(dst, src, len));
-    else
-    {
-        while (len--)
-            *(d + len) = *(s + len);
-    }
-    return (dst);
+	d = (char *)dst;
+	s = (char *)src;
+	if (d < s)
+		return (ft_memcpy(dst, src, len));
+	else
+	{
+		while (len--)
+			*(d + len) = *(s + len);
+	}
+	return (dst);
 }
