049d7515f2b6cd33868cf0ec0bb025a18f9c47ca
veryheavypickle <xavier.carroll01@gmail.com>
2022-06-05 21:30:56 +0200

    added comment on calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,11 +6,13 @@
 /*   By: xcarroll <xcarroll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 21:24:26 by xcarroll          #+#    #+#             */
-/*   Updated: 2022/06/05 19:05:49 by xcarroll         ###   ########.fr       */
+/*   Updated: 2022/06/05 21:30:31 by xcarroll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/* does not pass last test of libftTester*/
 
 void	*ft_calloc(size_t count, size_t size)
 {
