e9c99c3aa7773b8100c6509ed82feb32f526d20d
veryheavypickle <xavier.carroll01@gmail.com>
2022-07-05 05:01:30 +0200

    Updated strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len_s1;
+	size_t	len_s2;
+	char	*str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+	if (!str)
+		return (NULL);
+	ft_memcpy(str, s1, len_s1);
+	ft_memcpy(str + len_s1, s2, len_s2);
+	str[len_s1 + len_s2] = '\0';
+	return (str);
+}
