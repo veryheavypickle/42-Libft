279ff1823cb35d3cf7c0b86c71d23cc1311b5a5d
veryheavypickle <xavier.carroll01@gmail.com>
2022-07-05 20:14:44 +0200

    Fixed main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: xcarroll <xcarroll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 22:34:36 by xcarroll          #+#    #+#             */
-/*   Updated: 2022/07/05 20:11:50 by xcarroll         ###   ########.fr       */
+/*   Updated: 2022/07/05 20:14:27 by xcarroll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,6 @@ void print2d(char **arr)
 
 int	main(int argc, char **argv)
 {
+	ft_atoi(argv[argc - 1]);
 	return (0);
 }
