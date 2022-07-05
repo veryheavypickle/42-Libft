a71fe31bc2465f59fe313a2ce0d18dab758ba446
veryheavypickle <xavier.carroll01@gmail.com>
2022-07-05 20:23:36 +0200

    fixed an oopsie poopsie

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,35 +6,25 @@
 /*   By: xcarroll <xcarroll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 15:48:08 by xcarroll          #+#    #+#             */
-/*   Updated: 2022/06/05 17:35:41 by xcarroll         ###   ########.fr       */
+/*   Updated: 2022/07/05 20:23:20 by xcarroll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dest, const void *src, size_t len)
 {
-	int	i;
+	char	*dst;
+	char	*s;
 
-	if (!dst || !src)
-		return (NULL);
-	if (dst > src)
+	dst = (char *)dest;
+	s = (char *)src;
+	if (s < dst)
 	{
-		i = (int)len - 1;
-		while (i >= 0)
-		{
-			*(char *)(dst + i) = *(char *)(src + i);
-			i--;
-		}
+		while (len--)
+				*(dst + len) = *(s + len);
+		return (dest);
 	}
 	else
-	{
-		i = 0;
-		while (i < (int)len)
-		{
-			*(char *)(dst + i) = *(char *)(src + i);
-			i++;
-		}
-	}
-	return (dst);
+		return (ft_memcpy(dest, src, len));
 }
